72d5ee14862857c987db0af17566691ca60bee41
Dmitry Barykin <dmit.bar716@gmail.com>
2021-12-11 14:11:36 +0300

    Setup TypeScript/React/Webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "app/index.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devtool: "source-map",
+};
